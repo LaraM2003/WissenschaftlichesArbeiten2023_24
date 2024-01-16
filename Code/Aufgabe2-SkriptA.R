80a8315c1a080a761f69996206e446d2d5d2d752
LaraM2003 <lara.mierke@tu-dortmund.de>
2024-01-16 14:53:10 +0100

    Aufgabe2.a.v

--- Code/Aufgabe2-SkriptA.R
+++ Code/Aufgabe2-SkriptA.R
@@ -13,4 +13,13 @@
 plot(table(titanic_aufbereitet$Sex)) ### noch schöner machen
 
 
-## Grafik 2 - 
+## Grafik 2 - Verteilung Embarked
+plot(table(titanic_aufbereitet$Embarked)) ### noch schöner machen
+
+
+## Grafik 3 - Verteilung pClass
+plot(table(titanic_aufbereitet$Pclass)) ### noch schöner machen
+
+
+## Grafik 4 - Verteilung survived
+plot(table(titanic_aufbereitet$Survived)) ### noch schöner machen
