66772750cbc56bc1f1800980392faaec9ab40557
Alain Kauth <alain.kauth@gmx.de>
2024-02-24 10:59:15 +0100

    Umlaute umschreiben / sprechende Funktionsnamen

--- Code/Aufgabe2-SkriptA.R
+++ Code/Aufgabe2-SkriptA.R
@@ -11,18 +11,13 @@ library(gridExtra)
 # Lade Hilfsfunktionen
 source("Code/Aufgabe2-SkriptB.R")
 
-## An Alle: Bitte sprechende Funktionsnamen waehlen (und dann bitte ueberall im
-# Code aendern; auch in den anderen Skripts)
-## AK: ich wuerde die Beispielaufrufe auskommentieren oder besser loeschen.
-# Skripte sind so nicht alleinstehend lauffaehig bzw. werfen Fehler, da man 
-# davon aussgeht das der Datensatz geladen wurde.
 
 ###  i
-# Beschreibung: Eine Funktion, die deskriptive Statistiken fuer metrische
+# statsmetric:  Eine Funktion, die deskriptive Statistiken fuer metrische
 #               Variablen berechnet und ausgibt.
 # Input:        metrics - Vektor mit metrischen Daten
 # Output:       Angaben zum Minimum,Maximum, arithm.Mittel und Median der
-#               Werte.
+#               Werte; Dataframe mit numerischen Eintraegen
 
 
 # Funktion zur Berechnung mehrerer deskr.Statistiken fuer metrische Variablen
@@ -39,11 +34,13 @@ statsmetric <- function(metrics){
 }
 
 ###  ii
-# Beschreibung: Eine Funktion, die deskriptive Statistiken fuer kategoriale
+# statscategorials: 
+#               Eine Funktion, die deskriptive Statistiken fuer kategoriale
 #               Variablen berechnet und ausgibt.
 # Input:        categorials - Vektor mit kategorischen Daten
 # Output:       Angaben zu jedem einzigartigem Eintrag des Vektors. Dazu jeweils
-#               Anzahl und relative Haeufigkeit.
+#               Anzahl und relative Haeufigkeit; Dateframe mit numerischen 
+#               Eintraegen
 
 statscategorials <- function(categorials){
   # Dataframe zur Darstellung der Ergebnisse
@@ -56,44 +53,45 @@ statscategorials <- function(categorials){
 
 ### iii
 
-
-# Beschreibung: Eine Funktion, die passende deskriptive bivariate Statistiken 
+# Beschreibung: 
+# statsTwoCategorials:
+#               Eine Funktion, die passende deskriptive bivariate Statistiken 
 #               erstellt und ausgibt, um den Zusammenhang zwischen zwei 
 #               kategorialen Variablen zu analysieren.
-# Input:        kategorial1 & kategorial2 - Vektor, der die Ausprägungen eines 
-#               kategorialen Merkmals enthält.
-# Output:       result ## AK: Was sagen denn die Tabellen aus?
+# Input:        kategorial1 & kategorial2 - Vektor, der die Ausprasgungen eines 
+#               kategorialen Merkmals enthaslt.
+# Output:       Kreuztabelle und Statistiken des T-Quadrat-Tests
 
 
-iii <- function(kategorial1, kategorial2){
+statsTwoCategorials <- function(kategorial1, kategorial2){
   
   # Erstellung einer Kreuztabelle
   cross_table <- table(kategorial1, kategorial2)
   print(cross_table)
   
   
-  #Durchführung des Chi-Quadrat-Tests
+  #Durchfuehrung des Chi-Quadrat-Tests
   chi_square_test <- chisq.test(cross_table)
   print(chi_square_test)
 }
 
 
-
 ### iv
-## AK: Umlaute bitte mit ae, oe, ue umschreiben
-# Beschreibung: Funktion, welche für zwei Variablen eine deskriptive bivariate 
+# Beschreibung: 
+# statsDichoMetric:
+#               Funktion, welche fuer zwei Variablen eine deskriptive bivariate 
 #               Statistik berechnet und ausgibt
-# Input:        metrisch - Vektor mit Merkmalsausprägungen eines metrischen
+# Input:        metrisch - Vektor mit Merkmalsausprasgungen eines metrischen
 #                          Merkmals
-#               dichotom - Vektor mit Merkmalsausprägungen eines dichotomen
+#               dichotom - Vektor mit Merkmalsausprasgungen eines dichotomen
 #                          Merkmals
-# Output:       result   - ## AK: Was sagt mir denn die ausgegebene Tabelle?
+# Output:       Tabelle mit Kennzahlen (Mittelwert, Varianz und Std.abw.) sowie
+#               Boxplots (gruppiert nach der dich. Variable).
 
 
-
-iv <- function(metrisch, dichotom, labelX){
+statsDichoMetric <- function(metrisch, dichotom, labelX){
   
-  # prüft, ob beide Merkmale die gleiche Zahl an Beobachtungen haben
+  # prueft, ob beide Merkmale die gleiche Zahl an Beobachtungen haben
   if (length(metrisch) != length(dichotom)){
     stop("Die Vektoren sind unterschiedlich lang")
   }
@@ -129,21 +127,19 @@ iv <- function(metrisch, dichotom, labelX){
 
   
   return(list("Kennzahlen aufgeteilt nach dichotom" = tabelle))
-  
 }
 
 
-# Beispiel Aufruf
-iv(metrisch = titanic_aufbereitet$Age,
-   dichotom = titanic_aufbereitet$Survived, "Alter")
-
 ### v
 
-# Beschreibung: plottet für drei kategoriale Merkmal Balkendiagramme
+# Beschreibung: 
+# plotCategorials: 
+#               plottet fuer drei kategoriale Merkmal Balkendiagramme
 # Input:        dataframe mit vier kategorialen Merkmalen, benannt "a" - "c"
-# output:       (Graphen) ## AK: Auch hier was zeigen mir die Plots? X-Y-Achsen?
+# output:       (3 Graphen) Die die absolute Anzahl der jeweiligen Kategorien
+#               darstellt.
 
-v <- function(kategorial, Name1, Name2, Name3){
+plotCategorials <- function(kategorial, Name1, Name2, Name3){
   
     # erstellt die 3 plots
     plot1 <- ggplot(kategorial) +
@@ -166,18 +162,15 @@ v <- function(kategorial, Name1, Name2, Name3){
 }
 
 
-# beispiel Aufruf
-v(data.frame("a" = titanic_aufbereitet$Survived, 
-             "b" = titanic_aufbereitet$Sex,
-             "c" = titanic_aufbereitet$Pclass))
-
-
 ### vi
 
-# Beschreibung: plottet die Alters- und Geschlechtsstrukturen
+# Beschreibung: 
+#   plotAgeSex: 
+#           plottet die Alters- und Geschlechtsstrukturen
 # Input:        -
-# output:       -
-vi <- function(){
+# output:   Absolute Anzahl an Passagieren (in Altersgruppen von 5 Jahren/ unter
+#           -teilt nah Geschlecht)
+plotAgeSex <- function(){
   ggplot(data = titanic_aufbereitet, aes(x = Age, fill = Sex)) + 
     geom_histogram(binwidth = 5, color = "black") +
     labs(
@@ -193,12 +186,15 @@ vi <- function(){
 
 ### vii
 
-# Beschreibung: plottet die Überleben und die Seite, auf der die Kabine lag
+# Beschreibung: 
+# plotRoom:
+#           plottet die Ueberleben und die Seite, auf der die Kabine lag
 # Input:        -
-# output:       -
+# output:   Anzahl an Ueberlebenden und Verstorbenen je nach Zimmerseite.
 
-vii <- function(){
+plotRoom <- function(){
   ggplot(data = titanic_aufbereitet, aes(x = Room_1, fill = Survived)) + 
     geom_bar(stat = "count", color = "black") +
     scale_fill_manual(values = c("red2", "red4"))
 }
+
